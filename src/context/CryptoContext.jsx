84c8e4b7ac30fca090b66ddccbbbac5b56c54fbe
Ernest Ponce <meponce@tip.edu.ph>
2026-02-21 03:24:32 +0800

    fix: resolve merge conflicts in CryptoContext.jsx

--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -1,14 +1,10 @@
 import { createContext, useState, useContext } from 'react';
-<<<<<<< HEAD
 import { useLocalStorage } from '../hooks/useLocalStorage';
-=======
->>>>>>> b183b24f9c08c45a421d0020f4b5ff7f2eaad21a
 
 const CryptoContext = createContext();
 
 export const CryptoProvider = ({ children }) => {
   const [coins, setCoins] = useState([]);
-<<<<<<< HEAD
   
   // Your two memory states
   const [currency, setCurrency] = useLocalStorage('appCurrency', 'USD');
@@ -27,12 +23,6 @@ export const CryptoProvider = ({ children }) => {
       currency, setCurrency, currencySymbol, 
       chartType, setChartType 
     }}>
-=======
-  const [currency, setCurrency] = useState('USD');
-
-  return (
-    <CryptoContext.Provider value={{ coins, setCoins, currency, setCurrency }}>
->>>>>>> b183b24f9c08c45a421d0020f4b5ff7f2eaad21a
       {children}
     </CryptoContext.Provider>
   );
